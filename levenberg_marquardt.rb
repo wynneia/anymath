9b80c67d317608ae24796fe60a5e10a1ac35ef3c
Fela <60063027+jdxm3i@users.noreply.github.com>
2024-02-13 05:02:44 +0700

    Create levenberg_marquardt.rb

---
+++ levenberg_marquardt.rb
@@ -0,0 +1,59 @@
+require 'matrix'
+
+def levenberg_marquardt(x_data, y_data, initial_guess, func, jacobian, lambda: 0.01, tolerance: 1e-6, max_iterations: 100)
+  parameters = initial_guess.dup
+  residual = Vector.elements(y_data.map.with_index { |y, i| y - func.call(x_data[i], parameters) })
+
+  iteration = 0
+  while iteration < max_iterations
+    jacobian_matrix = Matrix.rows(y_data.map.with_index { |_, i| jacobian.call(x_data[i], parameters) })
+    jacobian_transpose = jacobian_matrix.transpose
+
+    squared_residual = residual.map { |r| r**2 }
+    sum_squared_residual = squared_residual.reduce(:+)
+
+    jacobian_squared_residual = jacobian_matrix.transpose * squared_residual
+    sum_jacobian_squared_residual = jacobian_squared_residual.reduce(:+)
+
+    jacobian_squared = jacobian_transpose * jacobian_matrix
+    diagonal = Matrix.diagonal(*(1 + lambda) * jacobian_squared.diagonal)
+
+    delta_parameters = (jacobian_squared + diagonal).inverse * jacobian_transpose * residual
+
+    new_parameters = parameters + delta_parameters.to_a.flatten
+
+    new_residual = Vector.elements(y_data.map.with_index { |y, i| y - func.call(x_data[i], new_parameters) })
+    new_squared_residual = new_residual.map { |r| r**2 }
+    new_sum_squared_residual = new_squared_residual.reduce(:+)
+
+    if new_sum_squared_residual < sum_squared_residual
+      lambda /= 10
+      parameters = new_parameters
+      residual = new_residual
+    else
+      lambda *= 10
+    end
+
+    break if delta_parameters.norm < tolerance
+
+    iteration += 1
+  end
+
+  parameters
+end
+
+#example usage:
+x_data = [0, 1, 2, 3, 4, 5]
+y_data = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6]
+
+#define the model function and its Jacobian
+func = ->(x, parameters) { parameters[0] * Math.exp(parameters[1] * x) }
+jacobian = ->(x, parameters) { [Math.exp(parameters[1] * x), parameters[0] * x * Math.exp(parameters[1] * x)] }
+
+#initial guess for parameters
+initial_guess = [1.0, 0.1]
+
+#run the Levenberg-Marquardt algorithm
+parameters = levenberg_marquardt(x_data, y_data, initial_guess, func, jacobian)
+
+puts "Optimal parameters: #{parameters}"
